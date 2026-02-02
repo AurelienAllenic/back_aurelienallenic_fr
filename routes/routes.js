70432b9b4ddfac9221ee3e9d2201908e09cb9760
AurelienAllenic <aurelien.allenic@gmail.com>
2026-02-02 20:33:56 +0100

    (feat) added route to check messages

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,10 @@ const contactController = require("../controllers/contactController");
 const cvController = require("../controllers/cvController");
 const authController = require("../controllers/authController");
 const { uploadCvFields } = require("../middlewares/multer");
+const messageController = require("../controllers/messageController")
 
 router.post("/contact", contactController.handleContact);
+router.get("/messages", authController.requireAuth, messageController.findAllMessages); 
 
 router.get("/cv", cvController.getCv);
 router.put(
